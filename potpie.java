11fc0221e3313e766363f9e247955006781998a8
EPrice <20401800+EPrice000@users.noreply.github.com>
2023-04-07 13:25:12 -0400

    Add files via upload

---
+++ potpie.java
@@ -0,0 +1,8 @@
+public class potpie extends food{
+  //subclass (CHILD) of food (PARENT)
+  //You can overwrite the eat() method in the FOOD file, example:
+ void eat() {
+  System.out.println("This potpie is great");
+  }
+
+}
